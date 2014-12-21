a764902ea804b09841837b59cf2beccd8f154c53
Piotr <piotr.@wp.pl>
2014-12-21 19:13:42 +0100

    Testuje

--- src/Almot/PlatformBundle/Controller/DefaultController.php
+++ src/Almot/PlatformBundle/Controller/DefaultController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+    * Kontroller to jest
+    */
     public function indexAction($name)
     {
         return $this->render('AlmotPlatformBundle:Default:index.html.twig', array('name' => $name));
